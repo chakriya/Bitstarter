1dbbd53880c2aa4bc0029c6e65c8ff8cf475a757
chakriya bowman <chakriya.bowman@ciprd.org>
2013-07-17 06:09:40 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -9,8 +9,8 @@ var len = buffer1.write(fs.readFileSync('index.html','utf-8'));
 //var len = buffer1.write("str1");
 
 app.get('/', function(request, response) {
-  response.send(buffer1.toString('utf-8',0,len));
- response.send("\n tese");
+//  response.send(buffer1.toString('utf-8',0,len));
+ response.send("test");
 });
 
 var port = process.env.PORT || 5000;
