edb40e285cda8ec12638e3bfb7f98f01ed8efa3a
chakriya bowman <chakriya.bowman@ciprd.org>
2013-07-17 05:31:33 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,11 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buffer1 = new Buffer(1026);
-
-var len = buffer1.write(readFileSync('index.html'));
+var str1 = readFileSync('index.html');
+var len = buffer1.write("this is a test");
 
 app.get('/', function(request, response) {
   response.send(buffer1.toString('utf-8',0,buffer1.length));
+  response.send("\n" + str1);
 });
 
 var port = process.env.PORT || 5000;
