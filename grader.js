7dfc133057b8cb7be853a04db6ed6f4ab32eb27c
chakriya bowman <chakriya.bowman@ciprd.org>
2013-07-20 04:59:03 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -27,6 +27,7 @@ var cheerio = require('cheerio');
 var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://www.google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
